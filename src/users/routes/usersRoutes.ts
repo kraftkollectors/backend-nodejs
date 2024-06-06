82e09c1b976928615b5d0dee284dbabecd33b553
chucky-golden <chukschibyke92@gmail.com>
2024-06-06 09:15:29 +0100

    filter

--- src/users/routes/usersRoutes.ts
+++ src/users/routes/usersRoutes.ts
@@ -7,7 +7,19 @@ import multer from 'multer'
 import verifyToken from '../../middlewares/auth'
 
 let storage = multer.memoryStorage()
-let upload = multer({storage: storage})
+
+let fileFilter = (req: any, file: any, cb: any) => {
+    if (file.mimetype.startsWith('image/') || file.mimetype.startsWith('video/')) {
+        cb(null, true);
+    } else {
+        cb(new Error('Invalid file type, only images and videos are allowed!'), false);
+    }
+};
+
+let upload = multer({
+    storage: storage,
+    fileFilter: fileFilter
+})
 
 const router: Router = express.Router();
 
