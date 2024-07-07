0755b9309f08b140860d0121a1bc3f03560d0092
chucky-golden <chukschibyke92@gmail.com>
2024-07-07 10:04:26 +0100

    admin ads

--- src/admin/services/adsService.ts
+++ src/admin/services/adsService.ts
@@ -4,75 +4,13 @@ import Ads from '../../models/ads'
 import mongoose from 'mongoose';
 import Report from '../../models/report';
 import Users from '../../models/users';
-import { getFilteredReport } from '../../middlewares/calculateBound';
+import { getFilteredAdsForAdmin, getFilteredReport } from '../../middlewares/calculateBound';
 
 
 const AdsService = {
     getAds: async (query: any) => {
         try {
-            const resPerPage = 10
-            const currentPageNum = Number(query.page) || 1
-            const skip = resPerPage * (currentPageNum - 1)
-
-            const search = query.keyword ? {
-                title: {
-                    $regex: query.keyword,
-                    $options: 'i'
-                },
-                active: true
-            } : { active: true }
-
-            // Check if the email already exists
-            const existingRecords = await Ads.find(search, { active: true })
-            .limit(resPerPage)
-            .skip(skip)
-
-            if (!existingRecords || existingRecords.length === 0) {
-                return { 
-                    data: { 
-                        existingRecords,
-                        totalDocuments: 0,
-                        hasPreviousPage: false, 
-                        previousPages: 0, 
-                        hasNextPage: false,      
-                        nextPages: 0,
-                        totalPages: 0,
-                        currentPage: currentPageNum
-                    },  
-                    statusCode: 201, 
-                    msg: "Success" 
-                }
-            }           
-
-            // Count the total number of documents
-            const totalDocuments = await Ads.countDocuments({ active: true });
-
-            // Calculate the total number of pages
-            const totalPages = Math.ceil(totalDocuments / resPerPage);
-
-            // Determine if there are previous and next pages
-            const hasPreviousPage = currentPageNum > 1;
-            const hasNextPage = currentPageNum < totalPages
-
-            // Calculate the number of previous and next pages available
-            const previousPages = currentPageNum - 1;
-            const nextPages = (totalPages - currentPageNum) < 0 ? 0 : totalPages - currentPageNum;
-               
-
-            return { 
-                data: { 
-                    existingRecords,
-                    totalDocuments, 
-                    hasPreviousPage, 
-                    previousPages, 
-                    hasNextPage, 
-                    nextPages,                    
-                    totalPages,
-                    currentPage: currentPageNum
-                }, 
-                statusCode: 201, 
-                msg: "Success" 
-            }
+            return await getFilteredAdsForAdmin(query)
         } catch (error: any) {
             throw new Error(`Error fetching ad: ${error.message}`);
         }
--- src/middlewares/calculateBound.ts
+++ src/middlewares/calculateBound.ts
@@ -26,6 +26,98 @@ function getBoundingBox(latitude: any, longitude: any, radiusInKm: any) {
     };
 }
 
+export async function getFilteredAdsForAdmin(data: any) {
+    const query: any = {};
+    let page: any = data.page ? data.page : 1
+  
+    if (data.q) {      
+      query.$or = [
+        { title: { $regex: data.q, $options: 'i' } },
+        { description: { $regex: data.q, $options: 'i' } }
+      ]
+    }
+
+    if (data.category) {
+      query.category = data.category;
+    }
+  
+    if (data.subCategory) {
+      query.subCategory = data.subCategory;
+    }
+  
+    if (data.minPrice) {
+      query.estimatedPrice = { ...query.estimatedPrice, $gte: data.minPrice };
+    }
+  
+    if (data.maxPrice) {
+      query.estimatedPrice = { ...query.estimatedPrice, $lte: data.maxPrice };
+    }
+  
+    if (data.location) {
+      query.location = data.location;
+    }
+  
+    if (data.longitude && data.latitude && data.radius) {
+      const { maxLat, minLat, maxLon, minLon } = getBoundingBox(data.latitude, data.longitude, data.radius);
+      query.latitude = { $gte: minLat, $lte: maxLat };
+      query.longitude = { $gte: minLon, $lte: maxLon };
+    }
+  
+    // Default sorting
+    let sortOption: any = { createdAt: -1 };
+  
+    if (data.sort === 'lowest-price') {
+      sortOption = { estimatedPrice: 1 };
+    } else if (data.sort === 'highest-price') {
+      sortOption = { estimatedPrice: -1 };
+    } else if (data.sort === 'latest') {
+      sortOption = { createdAt: -1 };
+    } else if (data.sort === 'best-rating') {
+      sortOption = { rating: -1 };
+    }
+  
+    // Calculate the skip value for pagination
+    const skip = (page - 1) * 10;
+  
+    // Fetch the filtered and sorted ads with pagination
+    const existingRecords = await Ad.find(query).sort(sortOption).skip(skip).limit(10);
+    const totalDocuments = await Ad.countDocuments(query);
+    const totalPages = Math.ceil(totalDocuments / 10);
+
+    if (!existingRecords || existingRecords.length === 0) {
+        return { 
+            data: { 
+                existingRecords, 
+                totalDocuments: 0,
+                hasPreviousPage: false, 
+                previousPages: 0, 
+                hasNextPage: false,      
+                nextPages: 0,
+                totalPages: 0,
+                currentPage: page
+            },  
+            statusCode: 201, 
+            msg: "Success" 
+        }
+    }
+  
+    return {
+        data: {
+            existingRecords,
+            totalDocuments,
+            hasPreviousPage: page > 1,
+            previousPages: page > 1 ? page - 1 : 0,
+            hasNextPage: page < totalPages,
+            nextPages: page < totalPages ? page + 1 : 0,
+            totalPages,
+            currentPage: page,
+        },  
+        statusCode: 201, 
+        msg: "Success" 
+    };
+}
+
+
 export async function getFilteredAds(data: any) {
     const query: any = { active: true };
     let page: any = data.page ? data.page : 1
